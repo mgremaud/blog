f33d2b5d163a21af12582a5871b62ec13d5d321b
matthieu <mgremaud@leadersleague.com>
2019-10-23 15:54:37 +0200

    fix database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { ArticlesController } from './modules/articles/controller/articles.controller';
 import { ArticlesService } from './modules/articles/service/articles.service';
 import { ArticlesModule } from './modules/articles/module/articles.module';
+import {TypeOrmModule, TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+const optionsMysql: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3308,
+  username: 'root',
+  password: 'toor',
+  database: 'blog',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  keepConnectionAlive: true,
+};
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+      TypeOrmModule.forRoot(optionsMysql),
+      ArticlesModule,
+  ],
   controllers: [AppController, ArticlesController],
   providers: [AppService, ArticlesService],
 })
